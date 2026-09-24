36d5c9f2aed21a5462d09186f07e56177ed96750
mike-axiom-mir <miketobi90@gmail.com>
2026-09-24 05:06:36 +0200

    neural: export bound brain contract

--- neural/axm_brain/__init__.py
+++ neural/axm_brain/__init__.py
@@ -1,4 +1,13 @@
+from .contract import BoundBrain, BrainIOContract, Channel
 from .core import AXMBrain, BrainConfig, Experience
 from .state import BrainSnapshotError
 
-__all__ = ["AXMBrain", "BrainConfig", "Experience", "BrainSnapshotError"]
+__all__ = [
+    "AXMBrain",
+    "BrainConfig",
+    "Experience",
+    "BrainSnapshotError",
+    "BrainIOContract",
+    "Channel",
+    "BoundBrain",
+]
