b4df499d56b25e5923c07f4cda2763d13656c3ef
mike-axiom-mir <miketobi90@gmail.com>
2026-09-25 09:37:55 +0200

    fix: restore valid UC neural experience module

--- src/axm_uc/neural_experience.py
+++ src/axm_uc/neural_experience.py
@@ -1,1 +1,13 @@
-from .neural_experience_transport import observe_uc_experience, record_uc_experience, reset_experiment_diagnostics\nfrom .neural_coverage import refresh_uc_coverage\n\n__all__ = [\"observe_uc_experience\", \"record_uc_experience\", \"reset_experiment_diagnostics\", \"refresh_uc_coverage\"]\n
+from .neural_coverage import refresh_uc_coverage
+from .neural_experience_transport import (
+    observe_uc_experience,
+    record_uc_experience,
+    reset_experiment_diagnostics,
+)
+
+__all__ = [
+    "observe_uc_experience",
+    "record_uc_experience",
+    "reset_experiment_diagnostics",
+    "refresh_uc_coverage",
+]
